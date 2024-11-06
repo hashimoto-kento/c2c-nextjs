1eebeda1908ca180717d95154b811ad1094bcde1
DESKTOP-J40HMMS\Owner <bug.bee.ron@gmail.com>
2024-11-07 07:23:29 +0900

    user page and error page add

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+// app/not-found.tsx
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <h1>404 - Page Not Found</h1>
+      <p>お探しのページは見つかりませんでした。</p>
+      <a href="/">ホームへ戻る</a>
+    </div>
+  );
+}
