ddadc13480cd1918b1666eabe8dd9e2e533a495a
DESKTOP-J40HMMS\Owner <bug.bee.ron@gmail.com>
2024-11-13 07:30:01 +0900

    auth page schedule

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,26 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../lib/prisma";
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   throw new Error("GOOGLE_CLIENT_ID is not set");
 }
 
 export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   callbacks: {
-    async session({ session }) {
+    async session({ session, user }) {
+      if (session.user) {
+      session.user.id = user.id;
+    }
       return session;
-    },
-    async jwt({ token }) {
-      return token;
     },
   },
 };
--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,16 +1,16 @@
 import { prisma } from "../../lib/prisma";
-// import { getServerSession } from "next-auth";
-// import { authOptions } from "../../lib/auth";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../lib/auth";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  // const session = await getServerSession(authOptions)
-  // if (!session?.user?.id) {
-  //   return new NextResponse('Unauthorized', { status: 401 })
-  // }
+  const session = await getServerSession(authOptions);
+  if (!session?.user?.id) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
 
   const todos = await prisma.todo.findMany({
-    // where: { userId: 'user1' }, // session.user.id
+    where: { userId: session.user.id },
     orderBy: { createdAt: "desc" },
   });
 
@@ -18,17 +18,17 @@ export async function GET() {
 }
 
 export async function POST(req: Request) {
-  try {
-    const { title } = await req.json();
-    const todo = await prisma.todo.create({
-      data: {
-        title,
-        userId: "user1", // session.user.id
-      },
-    });
-    return NextResponse.json(todo);
-  } catch (error) {
-    console.error("Error adding todo:", error);
-    return NextResponse.json({ error: 'Failed to create todo' }, { status: 500 });
+  const session = await getServerSession(authOptions);
+  if (!session?.user?.id) {
+    return new NextResponse("Unauthorized", { status: 401 });
   }
+
+  const { title } = await req.json();
+  const todo = await prisma.todo.create({
+    data: {
+      title,
+      userId: session.user.id,
+    },
+  });
+  return NextResponse.json(todo);
 }
