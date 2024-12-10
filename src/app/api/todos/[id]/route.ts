e4544f1fac42d3a257f33e1bdad8e4189ef9577c
DESKTOP-J40HMMS\Owner <bug.bee.ron@gmail.com>
2024-12-11 06:51:27 +0900

    build test

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -2,20 +2,25 @@ import prisma from "@/app/lib/prisma";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/lib/auth";
 import { NextRequest, NextResponse } from "next/server";
+import { type NextApiRequest, type NextApiResponse } from "next";
 
-export async function PUT(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
+interface PutContext {
+  params: {
+    id: string;
+  };
+}
+
+export async function PUT(request: Request | NextRequest, context: PutContext) {
   const session = await getServerSession(authOptions);
   if (!session?.user?.id) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
   try {
+    const { id } = context.params;
     const { title, completed } = await request.json();
     const todo = await prisma.todo.update({
-      where: { id: params.id },
+      where: { id },
       data: { title, completed },
     });
     return NextResponse.json(todo);
@@ -26,8 +31,8 @@ export async function PUT(
 }
 
 export async function DELETE(
-  request: NextRequest,
-  { params }: { params: { id: string } }
+  request: Request | NextRequest,
+  context: PutContext
 ) {
   const session = await getServerSession(authOptions);
   if (!session?.user?.id) {
@@ -35,8 +40,9 @@ export async function DELETE(
   }
 
   try {
+    const { id } = context.params;
     await prisma.todo.delete({
-      where: { id: params.id },
+      where: { id },
     });
     return NextResponse.json(null, { status: 204 });
   } catch (error) {
